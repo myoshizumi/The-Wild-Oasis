c7aa46fd1720d6c357d4d5759233509f7f9c7f64
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2023-08-07 22:53:14 +0900

    Setting Up Pages and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Heading from "./ui/Heading";
 import Input from "./ui/Input";
+import Row from "./ui/Row";
 
 const StyledApp = styled.main`
-	background-color: orangered;
+	/* background-color: orangered; */
 	padding: 20px;
 `;
 
@@ -14,15 +15,39 @@ function App() {
 		<>
 			<GlobalStyles />
 			<StyledApp>
-				<Heading as="h1">The Wild Oasis</Heading>
+				<Row>
+					<Row type="horizontal">
+						<Heading as="h1">The Wild Oasis</Heading>
+						<div>
+							<Heading as="h2">Check in and out</Heading>
+							<Button onClick={() => alert("Check in")}>
+								Check in
+							</Button>
+							<Button
+								variation="secondary"
+								size="small"
+								onClick={() => alert("Check out")}
+							>
+								Check out
+							</Button>
+						</div>
+					</Row>
 
-				<Heading as="h2">Check in and out</Heading>
-				<Button onClick={() => alert("Check in")}>Check in</Button>
-				<Button onClick={() => alert("Check out")}>Check out</Button>
+					<Row>
+						<Heading as="h3">Form</Heading>
 
-				<Heading as="h3">Form</Heading>
-				<Input type="number" placeholder="Number of guests" />
-				<Input type="number" placeholder="Number of guests" />
+						<form>
+							<Input
+								type="number"
+								placeholder="Number of guests"
+							/>
+							<Input
+								type="number"
+								placeholder="Number of guests"
+							/>
+						</form>
+					</Row>
+				</Row>
 			</StyledApp>
 		</>
 	);
