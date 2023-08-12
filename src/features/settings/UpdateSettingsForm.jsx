335d214751dd28da8a0f45578c101b315042184d
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2023-08-12 14:52:27 +0900

    Updating Application Settings

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -1,9 +1,11 @@
 /* eslint-disable no-unused-vars */
+import { updateSetting } from "../../services/apiSettings";
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
 import Spinner from "../../ui/Spinner";
 import { useSettings } from "./useSettings";
+import { useUpdateSetting } from "./useUpdateSetting";
 
 function UpdateSettingsForm() {
 	const {
@@ -16,7 +18,16 @@ function UpdateSettingsForm() {
 		} = {},
 	} = useSettings();
 
+	const { isUpdating, updateSetting } = useUpdateSetting();
+
 	if (isLoading) return <Spinner />;
+
+	function handleUpdate(e, field) {
+		const { value } = e.target;
+
+		if (!value) return;
+		updateSetting({ [field]: value });
+	}
 
 	return (
 		<Form>
@@ -25,27 +36,38 @@ function UpdateSettingsForm() {
 					type="number"
 					id="min-nights"
 					defaultValue={minBookingLength}
+					disabled={isUpdating}
+					onBlur={(e) => handleUpdate(e, "minBookingLength")}
 				/>
 			</FormRow>
+
 			<FormRow label="Maximum nights/booking">
 				<Input
 					type="number"
 					id="max-nights"
 					defaultValue={maxBookingLength}
+					disabled={isUpdating}
+					onBlur={(e) => handleUpdate(e, "maxBookingLength")}
 				/>
 			</FormRow>
+
 			<FormRow label="Maximum guests/booking">
 				<Input
 					type="number"
 					id="max-guests"
 					defaultValue={maxGuestsPerBooking}
+					disabled={isUpdating}
+					onBlur={(e) => handleUpdate(e, "maxGuestsPerBooking")}
 				/>
 			</FormRow>
+
 			<FormRow label="Breakfast price">
 				<Input
 					type="number"
 					id="breakfast-price"
 					defaultValue={breakfastPrice}
+					disabled={isUpdating}
+					onBlur={(e) => handleUpdate(e, "breakfastPrice")}
 				/>
 			</FormRow>
 		</Form>
