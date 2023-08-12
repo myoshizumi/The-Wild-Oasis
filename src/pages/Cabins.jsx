4593e27db45de21cfe653677a87c9ced6801219d
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2023-08-12 17:36:48 +0900

    Building a Modal Window Using a React Portal

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,13 +1,9 @@
-import { useState } from "react";
+import AddCabins from "../features/cabins/AddCabins";
 import CabinTable from "../features/cabins/CabinTable";
-import CreateCabinForm from "../features/cabins/CreateCabinForm";
-import Button from "../ui/Button";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 
 function Cabins() {
-	const [showForm, setShowForm] = useState(false);
-
 	return (
 		<>
 			<Row type="horizontal">
@@ -17,11 +13,7 @@ function Cabins() {
 
 			<Row>
 				<CabinTable />
-
-				<Button onClick={() => setShowForm((show) => !show)}>
-					Add new cabin
-				</Button>
-				{showForm && <CreateCabinForm />}
+				<AddCabins />
 			</Row>
 		</>
 	);
