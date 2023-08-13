f0a083fa7acda77171a81da42a842b4d15708e99
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2023-08-13 13:44:40 +0900

    Building a Reusable Context Menu

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react-refresh/only-export-components */
 import styled from "styled-components";
+import Menus from "../../ui/Menus";
 import Spinner from "../../ui/Spinner";
 import Table from "../../ui/Table";
 import CabinRow from "./CabinRow";
@@ -26,20 +27,25 @@ function CabinTable() {
 	if (isLoading) return <Spinner />;
 
 	return (
-		<Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
-			<Table.Header>
-				<div></div>
-				<div>Cabin</div>
-				<div>Capacity</div>
-				<div>Price</div>
-				<div>Discount</div>
-				<div></div>
-			</Table.Header>
-			<Table.Body
-				data={cabins}
-				render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
-			/>
-		</Table>
+		<Menus>
+			<Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
+				<Table.Header>
+					<div></div>
+					<div>Cabin</div>
+					<div>Capacity</div>
+					<div>Price</div>
+					<div>Discount</div>
+					<div></div>
+				</Table.Header>
+
+				<Table.Body
+					data={cabins}
+					render={(cabin) => (
+						<CabinRow cabin={cabin} key={cabin.id} />
+					)}
+				/>
+			</Table>
+		</Menus>
 	);
 }
 
