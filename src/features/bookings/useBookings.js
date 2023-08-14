2741bc65d122dfe2867dec035f109f0bfe6b5cbe
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2023-08-14 22:05:07 +0900

    API-Side Filtering:Filtering Bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,14 +1,26 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
 import { useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "react-router-dom";
 import { getBookings } from "../../services/apiBookings";
 
 export function useBookings() {
+	const [searchParams] = useSearchParams();
+
+	// Filter
+	const filterValue = searchParams.get("status");
+	const filter =
+		!filterValue || filterValue === "all"
+			? null
+			: { field: "status", value: filterValue };
+	//  : { field: "totalPrice", value: 5000, method: "gte" };
+
 	const {
 		isLoading,
 		data: bookings,
 		error,
 	} = useQuery({
-		queryKey: ["bookings"],
-		queryFn: getBookings,
+		queryKey: ["bookings", filter],
+		queryFn: () => getBookings({ filter }),
 	});
 	return { isLoading, error, bookings };
 }
