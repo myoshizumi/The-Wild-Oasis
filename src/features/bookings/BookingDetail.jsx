eb22b70f3f33f80b0c6bd66c45625cec00cc94b7
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2023-08-17 02:56:53 +0900

    Checking Out a Booking

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -13,6 +13,8 @@ import { useMoveBack } from "../../hooks/useMoveBack";
 import { useBooking } from "./useBooking";
 import Spinner from "../../ui/Spinner";
 import { useNavigate } from "react-router-dom";
+import { HiArrowUpOnSquare } from "react-icons/hi2";
+import { useCheckout } from "../check-in-out/useCheckout";
 
 const HeadingGroup = styled.div`
 	display: flex;
@@ -22,6 +24,8 @@ const HeadingGroup = styled.div`
 
 function BookingDetail() {
 	const { booking, isLoading } = useBooking();
+	const { checkout, isCheckingOut } = useCheckout();
+
 	const moveBack = useMoveBack();
 	const navigate = useNavigate();
 
@@ -53,6 +57,16 @@ function BookingDetail() {
 				{status === "unconfirmed" && (
 					<Button onClick={() => navigate(`/checkin/${bookingId}`)}>
 						Check in
+					</Button>
+				)}
+
+				{status === "checked-in" && (
+					<Button
+						icon={<HiArrowUpOnSquare />}
+						onClick={() => checkout(bookingId)}
+						disabled={isCheckingOut}
+					>
+						Check out
 					</Button>
 				)}
 
