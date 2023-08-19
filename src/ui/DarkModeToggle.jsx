d67af459bbf8092d604b64148d2a174d0b0d6e2c
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2023-08-19 16:14:59 +0900

    Implementing Dark Mode With CSS Variables

---
+++ src/ui/DarkModeToggle.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import { HiOutlineMoon, HiOutlineSun } from "react-icons/hi2";
+import { useDarkMode } from "../context/DarkModeContext";
+import ButtonIcon from "./ButtonIcon";
+
+function DarkModeToggle() {
+	const { isDarkMode, toggleDarkMode } = useDarkMode();
+
+	return (
+		<ButtonIcon onClick={toggleDarkMode}>
+			{isDarkMode ? <HiOutlineSun /> : <HiOutlineMoon />}
+		</ButtonIcon>
+	);
+}
+
+export default DarkModeToggle;
