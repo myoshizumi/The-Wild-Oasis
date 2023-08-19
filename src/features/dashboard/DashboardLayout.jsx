2393085dda12c1ee7d86b61218e514520907ba29
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2023-08-19 17:29:46 +0900

    Displaying Statistics

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable no-unused-vars */
 import styled from "styled-components";
 import Spinner from "../../ui/Spinner";
+import { useCabins } from "../cabins/useCabins";
+import Stats from "./Stats";
 import { useRecentBookings } from "./useRecentBookings";
 import { useRecentStays } from "./useRecentStays";
 
@@ -13,13 +15,24 @@ const StyledDashboardLayout = styled.div`
 
 function DashboardLayout() {
 	const { bookings, isLoading: isLoading1 } = useRecentBookings();
-	const { stays, confirmedStays, isLoading: isLoading2 } = useRecentStays();
+	const {
+		stays,
+		confirmedStays,
+		isLoading: isLoading2,
+		numDays,
+	} = useRecentStays();
+	const { cabins, isLoading: isLoading3 } = useCabins();
 
-	if (isLoading1 || isLoading2) return <Spinner />;
+	if (isLoading1 || isLoading2 || isLoading3) return <Spinner />;
 
 	return (
 		<StyledDashboardLayout>
-			<div>Statistics</div>
+			<Stats
+				bookings={bookings}
+				confirmedStays={confirmedStays}
+				numDays={numDays}
+				cabinCount={cabins.length}
+			/>
 			<div>Today&apos;s Activity</div>
 			<div>Chart stay duration</div>
 			<div>Chart sales</div>
