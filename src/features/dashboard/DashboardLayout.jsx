f454d206c3494ed1522691acf948dd2024f2f1cb
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2023-08-19 16:53:53 +0900

    Computing Recent Bookings and Stays

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,14 +1,22 @@
+/* eslint-disable no-unused-vars */
 import styled from "styled-components";
+import Spinner from "../../ui/Spinner";
+import { useRecentBookings } from "./useRecentBookings";
+import { useRecentStays } from "./useRecentStays";
 
 const StyledDashboardLayout = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  grid-template-rows: auto 34rem auto;
-  gap: 2.4rem;
+	display: grid;
+	grid-template-columns: 1fr 1fr 1fr 1fr;
+	grid-template-rows: auto 34rem auto;
+	gap: 2.4rem;
 `;
 
-
 function DashboardLayout() {
+	const { bookings, isLoading: isLoading1 } = useRecentBookings();
+	const { stays, confirmedStays, isLoading: isLoading2 } = useRecentStays();
+
+	if (isLoading1 || isLoading2) return <Spinner />;
+
 	return (
 		<StyledDashboardLayout>
 			<div>Statistics</div>
