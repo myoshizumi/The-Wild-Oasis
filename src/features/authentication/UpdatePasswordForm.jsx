4c6dbf56e29d80a3be68284a6e6ec214d72ddfbc
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2023-08-19 15:07:23 +0900

    Updating User Data and Password

--- src/features/authentication/UpdatePasswordForm.jsx
+++ src/features/authentication/UpdatePasswordForm.jsx
@@ -17,49 +17,50 @@ function UpdatePasswordForm() {
   }
 
   return (
-    <Form onSubmit={handleSubmit(onSubmit)}>
-      <FormRow
-        label="Password (min 8 characters)"
-        error={errors?.password?.message}
-      >
-        <Input
-          type="password"
-          id="password"
-          autoComplete="current-password"
-          disabled={isUpdating}
-          {...register("password", {
-            required: "This field is required",
-            minLength: {
-              value: 8,
-              message: "Password needs a minimum of 8 characters",
-            },
-          })}
-        />
-      </FormRow>
+		<Form onSubmit={handleSubmit(onSubmit)}>
+			<FormRow
+				label="New Password (min 8 chars)"
+				error={errors?.password?.message}
+			>
+				<Input
+					type="password"
+					id="password"
+					autoComplete="current-password"
+					disabled={isUpdating}
+					{...register("password", {
+						required: "This field is required",
+						minLength: {
+							value: 8,
+							message: "Password needs a minimum of 8 characters",
+						},
+					})}
+				/>
+			</FormRow>
 
-      <FormRow
-        label="Confirm password"
-        error={errors?.passwordConfirm?.message}
-      >
-        <Input
-          type="password"
-          autoComplete="new-password"
-          id="passwordConfirm"
-          disabled={isUpdating}
-          {...register("passwordConfirm", {
-            required: "This field is required",
-            validate: (value) =>
-              getValues().password === value || "Passwords need to match",
-          })}
-        />
-      </FormRow>
-      <FormRow>
-        <Button onClick={reset} type="reset" variation="secondary">
-          Cancel
-        </Button>
-        <Button disabled={isUpdating}>Update password</Button>
-      </FormRow>
-    </Form>
+			<FormRow
+				label="Confirm password"
+				error={errors?.passwordConfirm?.message}
+			>
+				<Input
+					type="password"
+					autoComplete="new-password"
+					id="passwordConfirm"
+					disabled={isUpdating}
+					{...register("passwordConfirm", {
+						required: "This field is required",
+						validate: (value) =>
+							getValues().password === value ||
+							"Passwords need to match",
+					})}
+				/>
+			</FormRow>
+			<FormRow>
+				<Button onClick={reset} type="reset" variation="secondary">
+					Cancel
+				</Button>
+				<Button disabled={isUpdating}>Update password</Button>
+			</FormRow>
+		</Form>
   );
 }
 
